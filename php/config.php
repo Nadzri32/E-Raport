5d9b6f26c3a55f6acac1fe9a9cd3234ed45c15ca
Nadzri <nadzrisan32@gmail.com>
2024-12-12 13:10:26 +0700

    pengembalian data config.php

--- php/config.php
+++ php/config.php
@@ -1,5 +1,5 @@
 <?php
-$dbhost ='nadzri32.github.io';
+$dbhost ='localhost';
 $dbuser ='root';
 $dbpass ='';
 $dbname ='rapotonline';
@@ -12,13 +12,13 @@ try {
 $con=mysqli_connect($dbhost,$dbuser,$dbpass,$dbname);
 ($GLOBALS["___mysqli_ston"] = mysqli_connect($dbhost, $dbuser, $dbpass));mysqli_select_db($GLOBALS["___mysqli_ston"], $dbname);
 /*css.plugin.hancon <?php echo $base; ?>*/
-$base='https://nadzri32.github.io/E-Raport/';
+$base='http://localhost/rapotonline/';
 /*control(link.redirect) <?php echo $basecon; ?>*/
-$basead='https://nadzri32.github.io/E-Raport/admin/';
+$basead='http://localhost/rapotonline/admin/';
 /*kelas(link.redirect) <?php echo $basekel; ?>*/
-$basegu='https://nadzri32.github.io/E-Raport/guru/';
-$basewa='https://nadzri32.github.io/E-Raport/walikelas/';
-$basewam='https://nadzri32.github.io/E-Raport/walimurid/';
+$basegu='http://localhost/rapotonline/guru/';
+$basewa='http://localhost/rapotonline/walikelas/';
+$basewam='http://localhost/rapotonline/walimurid/';
 
 $aplikasi=mysqli_fetch_array(mysqli_query($con,"SELECT * FROM aplikasi limit 1"));
 $ata=mysqli_fetch_array(mysqli_query($con,"SELECT * FROM tahunakademik where status='aktif' ")); $c_ta=$ata['c_ta'];
